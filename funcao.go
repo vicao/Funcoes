79be7e32696be660b4b2052e967770f9ed3390f3
Vinicius Silva <vinycd@gmail.com>
2026-02-03 20:58:17 -0300

    Primeira aula, sobre funções

---
+++ funcao.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func media(lista []float64) float64 {
+	total := 0.0
+	for _, nota := range lista {
+		total += nota
+	}
+	return total / float64(len(lista))
+}
+
+func main() { //programa que calcula a media de uma sala
+	lista := []float64{98, 93, 77, 82, 83} //lista de alunos
+	fmt.Println("A média da sala é:", media(lista))
+
+}
